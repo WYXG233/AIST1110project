2b34d68f06fa49855c34cf1653616a9948bb7d62
WYXG233 <100673426+WYXG233@users.noreply.github.com>
2022-12-25 14:30:26 +0800

    Add files via upload

---
+++ gym_maze/wrappers/__init__.py
@@ -0,0 +1,3 @@
+from maze_env.wrappers.clip_reward import ClipReward
+from maze_env.wrappers.discrete_actions import DiscreteActions
+from maze_env.wrappers.relative_position import RelativePosition
